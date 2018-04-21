442a58a59dea2ba4915a3464b6cf4a3a21da4a13
Veha Suwatphisankij <sky.48478@gmail.com>
2018-04-21 14:17:31 +0700

    add-index

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -5,4 +5,31 @@
   (dapi/nrepl-select :browser))
 
 (comment
+  (cljs-repl)
+  (in-ns 'test-ns)
+  ;(ns 'test-ns)
+  (+ 1 2)
+  (filter even? (take 1e3 (drop 1e3 (range))))
+  - > >
+  (->> (range)
+       (drop 1e3)
+       (take 1e3)
+       (filter even?)
+       (map inc))
+
+  (macroexpand-1 '(->> (drop 2 (range))))
+
+  (set! *print-length* 10)
+  *ns*
   (cljs-repl))
+
+
+(def hmap {:a :b})
+hmap
+(assoc hmap :k :v)
+
+(def aa (atom 1))
+aa
+(deref aa)
+@aa
+(reset! aa 2)
--- src/v1/view.cljs
+++ src/v1/view.cljs
@@ -8,15 +8,39 @@
   ;(println "title")
   [:li {:style {:color "green"
                 :padding "10px"
-                :padding-left "20px"
-                :list-style-type "decimal"}}
+                :padding-left "20px"}}
+                ;:list-style-type "decimal"}}
        (:title m)])
+
+(defn add-index [{:keys [title] :as t} index]
+  (assoc t :title (str index " " title)))
+
+(=
+  (add-index {:title "a" :key "1"} 1)
+  {:title "1 a" :key "1"})
 
 (defn main []
   [frame/page-view
    {:header "basic todo list with list of text"
     :goal db/goal
-    :content
-     [:ul
-      (for [[t index] (util/+index db/titles)]
-          [title (assoc t :key index)])]}])
+    :content [:div.nest "hi"
+              [:div.nest "l1 1"
+               [:div.nest "l1 2"]
+               [:h1.nest "l2 1"]
+               ;[title {:title "test title"}]
+               [:ul
+                (for [[t index] (util/+index db/titles)]
+                  [title (assoc (add-index t (inc index))
+                           :key index)])]]]}])
+(comment
+  (+ 1 2)
+  (+ 4 
+    (+ 2 3234234))
+  (- 2 3)
+  ((fn [a])
+   (+ a 1)
+   2)
+  (println "test")
+  (util.core/+index [2 2 2])
+  (js/alert))
+  
